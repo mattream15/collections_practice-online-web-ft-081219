f3a5190c1ad92f18523f817fee0f0fd8853c69f4
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-25 23:22:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(array)
+array.sort do |a, b|
+  a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+  b <=> a
+  end
+end
+
+def sort_array_char_count(values)
+values.sort do |left, right| 
+  left.length <=> right.length
+  end
+end
+
+def swap_elements(a, b)
+  a,b = b,a 
+  return a,b 
+end
+  
+#   array = [ ]
+# array[1], array[2] = array[2], array[1]
+# array
+# end
+
 
